9cf2bac5eb11070110ff538f97302b64be72f39a
slukas111 <sashamati@gmail.com>
2020-04-09 11:10:54 -0700

    get astronauts function to get names

--- iss.py
+++ iss.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
+import requests
 
-__author__ = '???'
+__author__ = 'Sasha Lukas'
+
+"""global scopes for project creating variable for json file"""
+url = 'http://api.open-notifty.org'
+icon = 'iss.gif'
+world_map ='map.gif'
+
+def get_astronauts():
+    astronauts = requests.get(url + '/astros.json')
+    astronauts.raise_for_status()
+
+    for person in astronauts["people"]:
+        print("  {} is currently on the {}.".format(
+            person["name"], person["craft"]))
+    print("There are {} people on the ISS right now.".format(
+        len(astronauts["people"])))
+    return astronauts.json()['people']
 
 
 def main():
